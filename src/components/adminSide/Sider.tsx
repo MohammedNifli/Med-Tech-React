f2a12c754f01450cf4e22f402bb3d6072853dfba
Mohammed Nifli <mohammednifliap@gmail.com>
2024-12-27 11:28:55 +0530

    logout issue

--- src/components/adminSide/Sider.tsx
+++ src/components/adminSide/Sider.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { RxDashboard } from "react-icons/rx";
 import { FaUsers, FaUserMd, FaCalendarAlt, FaBed, FaSignOutAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axiosInstance from "@/utils/axiosClient";
